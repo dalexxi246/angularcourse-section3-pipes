44602209a6e4014ebd30ba73b34eedc737a3391a
Wilmer Hurtado <walex6hh@gmail.com>
2018-07-15 23:08:37 -0500

    New pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'Wilmer';
@@ -11,4 +10,19 @@ export class AppComponent {
   shortDecimal = 1.2;
   percentageMin = 0.2;
   percentage = 0.213456789;
+  date = new Date();
+
+  objeto = {
+    nombre : 'wilmer',
+    apellido : 'hurtado',
+    seriesFavoritas : [{id: 1, nombre: 'Dragon Ball Z'}, {id: 2, nombre: 'One Piece'}, {id: 3, nombre: 'Sailor Moon'}]
+  };
+
+  greeting = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Llego la data');
+    }, 1500);
+  });
+
+  constructor() {  }
 }
